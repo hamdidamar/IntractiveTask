a25c5bad2789457ea5b4408c24cc9392dd0d4e70
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2025-07-13 23:02:49 +0400

    layers

---
+++ IntractiveTask/IntractiveTask.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntractiveTask.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
